fe9be8e913fb2ab5cf02059b3a43c5d3156e4e44
Marcos Souza <marcos@fontedeprecos.com.br>
2020-09-15 23:15:25 -0300

    minhas respostas desafio 1

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: "#desafio",
+    data: {
+        nome: "Marcos Souza",
+        idade: 29,
+        urlImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRlETbfD9BAqR_gi_BPfMOOrAhfP4Opc-Tw0TvqgxCH2BMjpHTg4lruG4psxu9_9_GuHPOvU3c77TMlAtRgIF8XHQ&usqp=CAU&ec=45702847"
+        
+    },
+    methods: {
+        idade3x() {
+            return parseInt(this.idade) * 3
+        },
+        random() {
+            return Math.random()
+        }
+    }
+})
