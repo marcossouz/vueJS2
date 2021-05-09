372e2507a154163cb1d175c50f354ec1f21b429e
Marcos Souza <mrcsz.m@gmail.com>
2021-05-08 21:07:55 -0300

    componentes

---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="contadores">
+        <h1>Contadores</h1>
+        <app-contador v-for="c in 5" :key="c" />
+        <button>?</button>
+    </div>
+</template>
+
+<script>
+import Contador from './Contador'
+    export default {
+        components: { 'app-contador': Contador }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
